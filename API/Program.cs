243cee94a68c64cc935562a1ebe303c854f5b27e
Mouloud Ould Ali <mouloudouldali78@gmail.com>
2022-12-25 22:26:36 -0500

    DTO Login DTO Register and Token

--- API/Program.cs
+++ API/Program.cs
@@ -1,9 +1,12 @@
 using API.Data;
+using API.interfaces;
+using API.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(opt =>
@@ -12,7 +15,7 @@ builder.Services.AddDbContext<DataContext>(opt =>
 });
 
 builder.Services.AddCors();
-
+builder.Services.AddScoped<ITokenService, TokenService>();
 
 var app = builder.Build();
 
