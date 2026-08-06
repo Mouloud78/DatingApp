278f72416b1d2cac658ca15e0fff7e9352ccc0bc
Mouloud78 <mouloudouldali78@gmail.com>
2026-08-06 16:19:15 -0400

    creating an API controller for members

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b02af695177b837236f67cca3d0c2a9693211e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a6b44d61dfec3c0df4fabd86f744e8c71a55d9d")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
