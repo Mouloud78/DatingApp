0b02af695177b837236f67cca3d0c2a9693211e7
Mouloud78 <mouloudouldali78@gmail.com>
2026-08-06 15:10:14 -0400

    Creating the EF and a new migration with EF

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aab2e9844aefa2196fc331ae39717452944ce2dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28b8db549a1151db29a27c240b68b679ca0971a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
